1f0b1261afe1f786502088baeb34e5dc0e0e1f0f
H-Khan-14 <haseebkhan14@hotmail.com>
2021-09-09 17:10:53 -0400

    Updates

---
+++ precache-manifest.34a0a90e38d3a11fa09439fbe43fe229.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd1a35a0534f95f3533a0c0e9a995c7",
+    "url": "/React-Redux-Translate/index.html"
+  },
+  {
+    "revision": "e6b9c411faaca46faa15",
+    "url": "/React-Redux-Translate/static/js/2.505cc725.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-Redux-Translate/static/js/2.505cc725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb21d53ac92fa0aba586",
+    "url": "/React-Redux-Translate/static/js/main.08ed70a9.chunk.js"
+  },
+  {
+    "revision": "35b709a0b95372e50a3a",
+    "url": "/React-Redux-Translate/static/js/runtime-main.6d66623d.js"
+  }
+]);
